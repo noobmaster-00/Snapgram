64b74ee132f3ad0846ab2c7b238a3e57c6f0d879
noobmaster-00 <adityarandhawa2001@gmail.com>
2024-04-15 14:28:51 +0530

    Initial commit

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,128 @@
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+import { Link } from 'react-router-dom'
+import { useToast } from "@/components/ui/use-toast";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
+import {Button} from "@/components/ui/button"
+import {useForm} from "react-hook-form"
+import {SignupValidation} from "@/lib/Validation"
+import Loaderr from "@/components/shared/Loaderr"
+import { createUserAccount } from "@/lib/appwrite/api"
+
+
+const SignupForm = () => {
+    const { toast } = useToast()
+    const isLoading = false;
+    // 1. Define your form.
+    const form = useForm < z.infer < typeof SignupValidation >> ({
+        resolver: zodResolver(SignupValidation),
+        defaultValues: {
+            name: '',
+            username: '',
+            email: '',
+            password: ''
+        }
+    })
+
+    // 2. Define a submit handler.
+    async function onSubmit(values: z.infer<typeof SignupValidation>) {
+      
+        const newUser = await createUserAccount(values);
+
+        if (!newUser) {
+            toast({ title: "Sign up failed. Please try again.", });
+            
+            return;
+          }
+
+          //const session  = await signinAccount()
+        
+    }
+
+    return (
+
+        <Form {...form}>
+            <div className="sm:w-420  flex-center flex-col">
+                <img src="/assets/images/logo.svg" alt="logo"/>
+
+                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new Account</h2>
+                <p className="text-light-3 small-medium md:base-regular mt-2 ">To use Snapgram, Please enter your detials</p>
+
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input type="text" className="shad-input" {...field}/>
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}/>
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input type="text" className="shad-input" {...field}/>
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}/>
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type="email" className="shad-input" {...field}/>
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}/>
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" className="shad-input" {...field}/>
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}/>
+
+                    <Button type="submit" className="shad-button_primary">
+                    {isLoading? (
+                      <div className ="flex-center gap-2">
+                        <Loaderr /> Loading...
+
+                      </div>
+                    ): "Sign up"}
+                    </Button>
+
+                    <p className="text-small-regular text-light-2 text-center mt-2">
+                        Already have an acount?
+                        <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+                    </p>
+                </form>
+            </div>
+        </Form>
+    )
+}
+
+export default SignupForm
---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,59 @@
+import { ID } from "appwrite";
+
+import { appwriteConfig, account, databases, storage, avatars } from "./config";
+import {  INewUser} from "@/types";
+
+// ============================================================
+// AUTH
+// ============================================================
+
+// ============================== SIGN UP
+export async function createUserAccount(user: INewUser) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+// ============================== SAVE USER TO DB
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
+}) {
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
+}
